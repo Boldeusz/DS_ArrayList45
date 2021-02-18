664a73fcd22545110c6bc1dae6782508a74f8c73
Boldeusz <mnpower@poczta.onet.pl>
2021-02-18 11:01:01 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Marcin", "Nienajadlo", 123));
+        employeeList.add(new Employee("Agata", "Yatta", 456));
+        employeeList.add(new Employee("Jan", "Kowalski", 789));
+        employeeList.add(new Employee("Frank", "Murarski", 763));
+
+//        employeeList.forEach(employee -> System.out.println(employee));
+
+//        System.out.println(employeeList.get(3));
+//        System.out.println(employeeList.size());
+        employeeList.add(2,new Employee("Michal", "Janowski",9870));
+//        employeeList.forEach(System.out::println);
+
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+        for (Employee employee: employeeArray){
+            System.out.println(employee);
+        }
+
+    }
+}
